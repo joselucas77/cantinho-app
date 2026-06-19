25fac7e6320871c8337fbf695e8f0f8c70d517af
BSM-Code-777 <bruno.sm.code@hotmail.com>
2026-06-19 19:32:02 -0300

    alteração do api.js

--- api.js
+++ api.js
@@ -1,8 +1,11 @@
 // api.js
-import axios from "axios";
+import axios from 'axios';
+
+// Mantenha o seu IP aqui (o modo Tunnel do Expo se encarrega de mapear a rota interna)
+const IP_COMPUTADOR = '192.168.0.5'; // Use o IP que você encontrou da rede wi-fi, quando mudar de rede pode ser necessario alterar o ip, pois o sistema pode travar
 
 const api = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `http://${IP_COMPUTADOR}:3000`,
   timeout: 5000, // Se o backend não responder em 5 segundos, o app cancela e avisa
 });
 
